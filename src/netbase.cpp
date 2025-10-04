055d85aeb1f561d245eab5c9b196c0493cda75a1
twisterarmy <twisterarmy@project>
2025-10-04 07:28:52 +0300

    add brackets

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -691,7 +691,7 @@ bool CNetAddr::IsTor() const
 // https://yggdrasil-network.github.io/2018/07/28/addressing.html#addressing-in-yggdrasil
 bool CNetAddr::IsYggdrasil() const
 {
-    return IsIPv6() && ip[0] == 0x02 || ip[0] == 0x03;
+    return IsIPv6() && (ip[0] == 0x02 || ip[0] == 0x03);
 }
 
 bool CNetAddr::IsLocal() const
